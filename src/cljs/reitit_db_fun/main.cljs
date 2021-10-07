89e9dd73b704d1d9e62473c67dccedc02eb068d2
rotaliator <rotaliator@gmail.com>
2021-10-07 19:24:39 +0200

    frontend form malli validation

--- src/cljs/reitit_db_fun/main.cljs
+++ src/cljs/reitit_db_fun/main.cljs
@@ -19,14 +19,60 @@
         (for [article articles]
           [:li (:title article)])))
 
-;; dodać lokalny stan z {} i walidować
-(rum/defc article-form < rum/static
-  []
-  [:form {:on-submit (fn [e] (.preventDefault e) (js/console.log e))}
-   [:input.input {:name :title :type :text :placeholder "Tytuł"}]
-   [:textarea.textarea {:name :body :type :textarea :placeholder "Treść"}]
-   [:button.button.is-primary {:type :submit} "Zapisz"]])
+(rum/defc input < rum/static
+  [{:keys [type name label message value on-change]}]
+  (let [inputs
+        {:text
+         [:div.field
+          [:label.label label]
+          [:div.control
+           [:input.input {:value       value
+                          :type        "text"
+                          :placeholder label
+                          :on-change   on-change}]]
+          [:p.help.is-danger message]]
+         :text-area
+         [:div.field
+          [:label.label label]
+          [:div.control
+           [:textarea.textarea {:name        name
+                                :type        :textarea
+                                :placeholder label
+                                :value       value
+                                :on-change   on-change}]]
+          [:p.help.is-danger message]]
+         }]
+    (get inputs type)))
 
+
+;; dodać lokalny stan z {} i walidować
+(rum/defcs article-form < (rum/local
+                           {:title "" :title-message ""
+                            :body  "" :body-message  ""}
+                           ::article-form)
+  [state]
+  (let [local-state            (::article-form state)
+        {:keys [title body
+                title-message
+                body-message]} @local-state]
+    [:div
+     (input {:name      :title :type :text :label "Tytuł" :value title
+             :message title-message
+             :on-change (fn [e] (swap! local-state assoc :title (-> e .-target .-value)))})
+     (input {:name      :body :type :text-area :label "Treść" :value body
+             :message body-message
+             :on-change (fn [e] (swap! local-state assoc :body (-> e .-target .-value)))})
+     [:button.button.is-primary {:on-click
+                                 (fn [_]
+                                   (if (m/validate v/Article {:title title :body body})
+                                     (prn "call to backend" {:title title :body body})
+                                     (let [errors (-> v/Article
+                                                      (m/explain {:title title :body body})
+                                                      (me/humanize))]
+                                       (doseq [[k v] errors]
+                                         (let [msg-key (-> k name (#(str % "-message")) keyword)]
+                                           (swap! local-state assoc msg-key v))))))}
+      "Zapisz"]]))
 
 (rum/defc app []
   [:div.container
@@ -42,10 +88,11 @@
 
   (-> v/Article
       (m/explain {:title     "tytuł"
-                  :body      "eść"
+                  :body      "treść"
                   :author-id "1"})
       (me/humanize {:errors (-> me/default-errors
                                 (assoc ::m/missing-key
                                        {:error/message "Pole wymagane"}))}))
+  (js/alert "asdsd")
 
   )
